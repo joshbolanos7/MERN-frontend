ff02d93c01298f2bc66db594152abc0832af8080
Joshua Bolanos <josh.bolanos7@gmail.com>
2021-10-19 18:50:03 -0400

    real push after some found bugs.

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 function Show(props) {
   const id = props.match.params.id;
   const people = props.people;
-  const person = people.find(p => p._id === id);
+  const person = people.find((p) => p._id === id);
 
-  // state for form
-  const [ editForm, setEditForm ] = useState(person);
+  const [editForm, setEditForm] = useState(person);
 
   // handleChange function for form
-  const handleChange = event => {
+  const handleChange = (event) => {
     setEditForm({ ...editForm, [event.target.name]: event.target.value });
-  }
+  };
 
-  // handlesubmit for form
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    props.updatePeople(editForm, person._id);
-    // redirect people back to index
-    props.history.push("/");
+    props.updatePeople(editForm);
+    props.history.push("/")
+  };
+
+  const removePerson = () => {
+      props.deletePeople(person._id)
+      props.history.push("/")
   }
 
   return (
@@ -25,6 +27,7 @@ function Show(props) {
       <h1>{person.name}</h1>
       <h2>{person.title}</h2>
       <img src={person.image} alt={person.name} />
+      <button id="delete" onClick={removePerson}>DELETE</button>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
